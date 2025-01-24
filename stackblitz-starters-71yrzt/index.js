c32a8a46dfb02524b7a48d12ad40103185f0f759
anujgoyaleng <anuj.goyal@kalvium.community>
2025-01-24 16:10:22 +0530

    upload

--- stackblitz-starters-71yrzt/index.js
+++ stackblitz-starters-71yrzt/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = 3000
 
 app.use(express.static('static'));
+
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
@@ -13,3 +21,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
